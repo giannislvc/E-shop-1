dd5f21a9c50d400868193dada2abf204a3a4f93f
vboudis <v.boudis1995@gmail.com>
2020-02-04 18:21:44 +0200

    Added NavDrawer Fragment

--- app/src/main/java/com/nativeboys/eshop/ui/main/MainFragment.java
+++ app/src/main/java/com/nativeboys/eshop/ui/main/MainFragment.java
@@ -38,7 +38,7 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_main_layout, container, false);
     }
 
     @Override
